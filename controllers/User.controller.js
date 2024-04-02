1326adc3c06245f0de8b6edff77df2adf2087897
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-04-02 20:45:31 +0530

    added getUserInformation controller function to return the search results starting with the search query

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -3,6 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const { hash } = require("bcryptjs");
 
 const User = require("../models/User.model");
+const Message = require("../models/Message.model");
+const GroupMessage = require("../models/GroupMessage.model");
 const CustomError = require("../errors/CustomError");
 
 const putEditProfile = async (req, res, next) => {
@@ -76,8 +78,80 @@ const getUserAccountInformation = async (req, res, next) => {
     }
 }
 
+const getUserConnections = async (req, res, next) => {
+    try {
+        const userId = req.userId;
+        const user = await User.findById(userId);
+        if (!user) {
+            throw new CustomError(StatusCodes.NOT_FOUND, "User not found.");
+        }
+        const connectedToUsers = user.connectedTo;
+        const groups = user.groups;
+        const messages = [];
+
+        // first we will take one to one chats then group
+        connectedToUsers.forEach(async id => {
+            let messages1;
+            let messages2;
+
+            // senderId = userId and recieverId = id
+            messages1 = await Message.find({ senderId: userId, recieverId: id }).sort({ createdAt: -1 }).populate("recieverId");
+
+            // senderId = id and recieverId = userId
+            messages2 = await Message.find({ senderId: id, recieverId: userId }).sort({ createdAt: -1 }).populate("senderId");
+
+            // comparing the index: 0 value to get the most recent message
+            const message = messages1[0].createdAt > messages2[0].createdAt ? {
+                userId: id,
+                profilePicUrl: messages1[0].reciever.profilePicUrl,
+                username: messages1[0].reciever.username,
+                description: messages1[0].description,
+                seen: messages1[0].seen,
+                delivered: messages1[0].delivered,
+                createdAt: messages1[0].createdAt
+            } : {
+                userId: id,
+                profilePicUrl: messages2[0].sender.profilePicUrl,
+                username: messages2[0].sender.username,
+                description: messages2[0].description,
+                seen: messages2[0].seen,
+                delivered: messages2[0].delivered,
+                createdAt: messages1[0].createdAt
+            };
+            messages.push(message);
+        });
+
+        // now taking group chats
+        groups.forEach(async id => {
+            const groupMessages = await GroupMessage.find({ groupId: id }).sort({ createdAt: -1 }).populate("group");
+            messages.push(groupMessages[0]);
+        });
+        res.status(StatusCodes.OK).json(messages);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getUserInformation = async (req, res, next) => {
+    try {
+        const { query } = req.query;
+        if (!query) {
+            throw new CustomError(StatusCodes.UNPROCESSABLE_ENTITY, "Don't provide empty search query.");
+        }
+        const users = await User.find({ username: { $regex: `^${query}` } });
+        if (!users) {
+            return res.sendStatus(StatusCodes.NOT_FOUND)
+        }
+        res.status(StatusCodes.OK).json(users);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
     putEditProfile,
     putEditAccountCredentials,
-    getUserAccountInformation
+    getUserAccountInformation,
+    getUserConnections,
+    getUserInformation
 }
