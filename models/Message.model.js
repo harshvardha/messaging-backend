421908c284c570f852a8b024458f1560277453ef
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-04-02 20:49:09 +0530

    minor syntax change

--- models/Message.model.js
+++ models/Message.model.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    senderId: {
+    sender: {
         type: mongoose.Types.ObjectId,
         required: true,
         ref: "User"
     },
-    recieverId: {
+    reciever: {
         type: mongoose.Types.ObjectId,
         required: true,
         ref: "User"
